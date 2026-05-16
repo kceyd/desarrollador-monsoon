a463f218a0836cc6fd93a66841a81180a21dc4c9
kceyd <ser.pizarrob@duocuc.cl>
2026-05-15 20:06:22 -0400

    sexta parte

---
+++ Desarrollador/desarollador/src/main/java/com/example/desarollador/DTO/DTOjuego.java
@@ -0,0 +1,5 @@
+package com.example.desarollador.service;
+
+public class DTOjuego {
+
+}
--- Desarrollador/desarollador/src/main/java/com/example/desarollador/service/servicioDesarrollador.java
+++ Desarrollador/desarollador/src/main/java/com/example/desarollador/service/servicioDesarrollador.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.desarollador.DTO.DTOjuego;
 import com.example.desarollador.model.DesarrolladorGG;
 import com.example.desarollador.repository.repoDesarrollador;
 
@@ -16,26 +17,46 @@ import jakarta.transaction.Transactional;
 public class servicioDesarrollador {
     @Autowired
     private repoDesarrollador desarrolladorRepository;
-
+ 
+    private final WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:8085/api/v0/juegos")
+            .build();
+ 
     public void crearDesarrollador(DesarrolladorGG desarrollador) {
         desarrolladorRepository.save(desarrollador);
     }
-
+ 
     public List<DesarrolladorGG> obtenerDesarrolladores() {
         return desarrolladorRepository.findAll();
     }
-
+ 
     public DesarrolladorGG obtenerDesarrollador(Long id) {
         return desarrolladorRepository.findById(id).orElse(null);
     }
-
+ 
     public void actualizarDesarrollador(DesarrolladorGG desarrollador) {
         desarrolladorRepository.save(desarrollador);
     }
-
+ 
     public void eliminarDesarrollador(Long id) {
         desarrolladorRepository.deleteById(id);
     }
+ 
+    public List<DTOjuego> obtenerTodosLosJuegos() {
+        return webClient.get()
+                .retrieve()
+                .bodyToFlux(DTOjuego.class)
+                .collectList()
+                .block();
+    }
+ 
+    public DTOjuego obtenerJuego(Long juegoId) {
+        return webClient.get()
+                .uri("/{id}", juegoId)
+                .retrieve()
+                .bodyToMono(DTOjuego.class)
+                .block();
+    }
+ 
 }
-
-
+   
