1ad57b0920634492494d5d0f6ed9031e2a372e42
kceyd <ser.pizarrob@duocuc.cl>
2026-05-15 20:09:39 -0400

    sexta partes

--- Desarrollador/desarollador/src/main/java/com/example/desarollador/DTO/DTOjuego.java
+++ Desarrollador/desarollador/src/main/java/com/example/desarollador/DTO/DTOjuego.java
@@ -1,5 +1,14 @@
-package com.example.desarollador.service;
+package com.example.desarollador.DTO;
 
+import lombok.Data;
+
+@Data
 public class DTOjuego {
+    private Long id;
+    private String titulo;
+    private String genero;
+    private String descripcion;
+    private Double precio;
+    private String desarrollador;
 
 }
--- Desarrollador/desarollador/src/main/java/com/example/desarollador/service/servicioDesarrollador.java
+++ Desarrollador/desarollador/src/main/java/com/example/desarollador/service/servicioDesarrollador.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.desarollador.DTO.DTOjuego;
 import com.example.desarollador.model.DesarrolladorGG;
